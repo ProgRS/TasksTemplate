df1fa172f89a3810f5cf63fc7e237b12b31b1c4f
ProgRs <lfernandotc@hotmail.com>
2022-07-17 16:25:51 -0300

    Implementação do Login

--- Fonte/app/src/main/java/com/devmasterteam/tasks/service/repository/remote/RetrofitClient.kt
+++ Fonte/app/src/main/java/com/devmasterteam/tasks/service/repository/remote/RetrofitClient.kt
@@ -29,5 +29,11 @@ class RetrofitClient private constructor(){
                 }
                 return  INSTANCE
             }
+
+            fun <T> getService(serviceClass: Class<T>) : T{
+
+                    return getRetrofitInstance().create(serviceClass)
+            }
+
     }
 }
