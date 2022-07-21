15b35090978bd3a454f29b56d161917b093c47a0
ProgRs <lfernandotc@hotmail.com>
2022-07-21 16:41:25 -0300

    Validação de conexão com a internet

--- Fonte/app/src/main/java/com/devmasterteam/tasks/service/repository/BaseRepository.kt
+++ Fonte/app/src/main/java/com/devmasterteam/tasks/service/repository/BaseRepository.kt
@@ -1,6 +1,10 @@
 package com.devmasterteam.tasks.service.repository
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.devmasterteam.tasks.R
 import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.APIListener
@@ -9,23 +13,22 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-open  class BaseRepository(val context: Context){
+open  class BaseRepository(val context: Context) {
 
-     fun failResponse(str: String) : String {
-        return  Gson().fromJson(str, String::class.java)
+    fun failResponse(str: String): String {
+        return Gson().fromJson(str, String::class.java)
 
     }
 
 
-
-    fun <T> executeCall(call: Call<T>, listener: APIListener<T>){
+    fun <T> executeCall(call: Call<T>, listener: APIListener<T>) {
 
         call.enqueue(object : Callback<T> {
 
             override fun onResponse(call: Call<T>, response: Response<T>) {
-                if(response.code() == TaskConstants.HTTP.SUCCESS){
+                if (response.code() == TaskConstants.HTTP.SUCCESS) {
                     response.body()?.let { listener.onSuccess(it) }
-                }else{
+                } else {
 
 
                     listener.onFailure(failResponse(response.errorBody()!!.string()))
@@ -37,5 +40,38 @@ open  class BaseRepository(val context: Context){
             }
 
         })
+    }
+
+
+    fun isConnectionAvailable(): Boolean {
+        var result = false
+
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            val activeNet = cm.activeNetwork ?: return false
+            val netWorkCapabilities = cm.getNetworkCapabilities(activeNet) ?: return false
+
+            result = when {
+
+                netWorkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                netWorkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+
+                else -> false
+            }
+
+        } else {
+
+            if (cm.activeNetworkInfo != null) {
+                result = when (cm.activeNetworkInfo!!.type) {
+                    ConnectivityManager.TYPE_WIFI -> true
+                    ConnectivityManager.TYPE_MOBILE -> true
+                    ConnectivityManager.TYPE_ETHERNET -> true
+                    else -> false
+                }
+            }
+        }
+        return result
     }
 }
