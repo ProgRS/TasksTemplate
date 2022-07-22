95bbd6a14ee0f4dcba7aeaeca5be0fdfcb2da94b
ProgRs <lfernandotc@hotmail.com>
2022-07-22 09:29:47 -0300

    Implementação filtros de tarefas expiradas e próximas

--- Fonte/app/src/main/java/com/devmasterteam/tasks/viewmodel/TaskListViewModel.kt
+++ Fonte/app/src/main/java/com/devmasterteam/tasks/viewmodel/TaskListViewModel.kt
@@ -1,10 +1,13 @@
 package com.devmasterteam.tasks.viewmodel
 
 import android.app.Application
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.TaskModel
 import com.devmasterteam.tasks.service.model.ValidationModel
@@ -15,6 +18,7 @@ class TaskListViewModel(application: Application) : AndroidViewModel(application
 
      private   val taskRepository = TaskRepository(application.applicationContext)
      private val priorityRepository = PriorityRepository(application.applicationContext)
+     private var taskFilter = 0
 
     private val _delete = MutableLiveData<ValidationModel>()
     val delete: LiveData<ValidationModel> = _delete
@@ -27,13 +31,15 @@ class TaskListViewModel(application: Application) : AndroidViewModel(application
     val  status: LiveData<ValidationModel> = _status
 
 
-    fun list(){
-        taskRepository.list(object : APIListener<List<TaskModel>>{
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun list(filter: Int){
+        taskFilter = filter
+        val listener = object : APIListener<List<TaskModel>>{
             override fun onSuccess(result: List<TaskModel>) {
 
-                    result.forEach{
-                        it.priorityDescription = priorityRepository.getDescription(it.priorityId)
-                    }
+                result.forEach{
+                    it.priorityDescription = priorityRepository.getDescription(it.priorityId)
+                }
 
                 _tasks.value = result
             }
@@ -42,13 +48,23 @@ class TaskListViewModel(application: Application) : AndroidViewModel(application
 
             }
 
-        })
+        }
+
+        when (filter) {
+            TaskConstants.FILTER.ALL ->  taskRepository.list(listener)
+            TaskConstants.FILTER.NEXT -> taskRepository.listNext(listener)
+            else -> taskRepository.listOverdue(listener)
+
+        }
+
+
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     fun delete(id: Int){
         taskRepository.delete(id, object : APIListener<Boolean>{
             override fun onSuccess(result: Boolean) {
-                list()
+                list(taskFilter)
             }
 
             override fun onFailure(message: String) {
@@ -59,10 +75,12 @@ class TaskListViewModel(application: Application) : AndroidViewModel(application
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     fun status(id: Int, complete: Boolean){
         val listener =  object : APIListener<Boolean>{
+            @RequiresApi(Build.VERSION_CODES.M)
             override fun onSuccess(result: Boolean) {
-                list()
+                list(taskFilter)
             }
 
             override fun onFailure(message: String) {
